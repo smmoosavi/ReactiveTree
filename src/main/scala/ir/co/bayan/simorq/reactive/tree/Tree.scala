b3d8b4eee0e51e1897f13344a65385ea5b4c55b2
Fahim Ayat <fahim.ayat@gmail.com>
2015-11-03 15:26:19 +0330

    Adds foreach

--- src/main/scala/ir/co/bayan/simorq/reactive/tree/Tree.scala
+++ src/main/scala/ir/co/bayan/simorq/reactive/tree/Tree.scala
@@ -3,7 +3,27 @@ package ir.co.bayan.simorq.reactive.tree
 /**
  * @author S.Hosein Ayat
  */
-trait Tree
+trait Tree {
 
-case class InnerNode(left : Tree , right : Tree) extends Tree
-case class Leaf(value : Int) extends Tree
+  def toList: List[Int]
+
+  def foreach(f: Int => Unit): Unit
+}
+
+case class InnerNode(left: Tree, right: Tree) extends Tree {
+
+  override def toList: List[Int] = {
+    left.toList ++ right.toList
+  }
+
+  override def foreach(f: (Int) => Unit): Unit = {
+    left foreach f
+    right.foreach(f)
+  }
+}
+
+case class Leaf(value: Int) extends Tree {
+  override def toList: List[Int] = value :: Nil
+
+  override def foreach(f: (Int) => Unit): Unit = f(value)
+}
--- src/test/scala/ir/co/bayan/simorq/reactive/tree/TreeTest.scala
+++ src/test/scala/ir/co/bayan/simorq/reactive/tree/TreeTest.scala
@@ -1,10 +1,24 @@
 package ir.co.bayan.simorq.reactive.tree
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 
 /**
  * @author S.Hosein Ayat
  */
-class TreeTest extends FlatSpec with Matchers{
+class TreeTest extends FlatSpec with Matchers {
+  implicit def convertToLeaf(value: Int): Leaf = Leaf(value)
+
+  val tree = InnerNode(Leaf(2), Leaf(3))
+  val tree2: InnerNode = InnerNode(InnerNode(2, InnerNode(1, 2)), InnerNode(4, 5))
+
+  "Tree" can "create an ordered list of it's elements" in {
+    tree.toList shouldBe List(2, 3)
+    tree2.toList shouldBe List(2 , 1 , 2 , 4 , 5)
+  }
+
+
+  it can "print all elements using foreach" in {
+    tree2 foreach println
+  }
 
 }
