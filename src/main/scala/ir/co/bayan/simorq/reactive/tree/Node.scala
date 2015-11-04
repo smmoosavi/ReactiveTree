b2f881a638a88f460ec9addba0ef03a1da9a8187
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2015-11-04 23:29:04 +0330

    implements toList on Node

--- src/main/scala/ir/co/bayan/simorq/reactive/tree/Node.scala
+++ src/main/scala/ir/co/bayan/simorq/reactive/tree/Node.scala
@@ -3,12 +3,18 @@ package ir.co.bayan.simorq.reactive.tree
 /**
  * @author S.Hosein Ayat
  */
-case class Node[T](value: T, left: Option[Node[T]], right: Option[Node[T]]) {
+case class Node[T](value: T, left: Option[Node[T]] = None, right: Option[Node[T]] = None) {
   /**
    * In fix traverse
    * @return list of Ts
    */
-  def toList: List[T] = ???
+  def toList: List[T] = {
+
+    val leftList: List[T] = if (left.isEmpty) Nil else left.get.toList
+    val middleList = value :: Nil
+    val rightList: List[T] = if (right.isEmpty) Nil else right.get.toList
+    leftList ++ middleList ++ rightList
+  }
 
   def map[U](f: T => U): Node[U] = ???
 
---
+++ src/test/scala/ir/co/bayan/simorq/reactive/tree/NodeTest.scala
@@ -0,0 +1,18 @@
+package ir.co.bayan.simorq.reactive.tree
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * @author Seyyed Morteza Moosavi
+  */
+class NodeTest extends FlatSpec with Matchers {
+  implicit def convertToNode[T](value: T): Node[T] = Node(value, None, None)
+
+  val tree = Node[Int](1, Some(2), Some(3))
+  val tree2 = Node[String]("1", Some("2"), Some("3"))
+
+  "Node" can "create an ordered list of it's elements" in {
+    tree.toList shouldBe List(2, 1, 3)
+    tree2.toList shouldBe List("2", "1", "3")
+  }
+}
